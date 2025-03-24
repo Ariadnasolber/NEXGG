4977768d7b854171eaba7dd445ab8b908bb4d6dd
Ariadnasolber <ariadnasolber@campus.monlau.com>
2025-03-24 18:52:05 +0100

    login-front

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,33 +10,33 @@ export default function Login() {
     };
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-200">
-            <div className="bg-white p-8 rounded-2xl shadow-xl w-96">
-                <h2 className="text-2xl font-semibold mb-6 text-center text-gray-700">Login</h2>
+        <div className="flex items-center justify-start h-screen bg-cover bg-center overflow-hidden" style={{ backgroundImage: "url('/talon-bg.jpg')" }}>
+            <div className="bg-[#0F1015]/60 px-30 shadow-xl w-160 h-screen pt-70 mt-[104px] rounded-tr-lg">
+                <h2 className="text-2xl font-semibold mb-6 text-center text-gray-300">Login</h2>
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
-                        <label className="block text-sm font-medium text-gray-600">Email</label>
                         <input
                             type="email"
-                            className="w-full p-3 border border-gray-300 rounded-lg mt-1 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                            className="w-full p-3 placeholder-gray-500 w-[402.24px] h-[50.08px] bg-[#16171b] rounded-sm border-[0.40px] border-[#363636]"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             required
+                            placeholder="Email"
                         />
                     </div>
                     <div>
-                        <label className="block text-sm font-medium text-gray-600">Password</label>
                         <input
                             type="password"
-                            className="w-full p-3 border border-gray-300 rounded-lg mt-1 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                            className="w-full p-3 placeholder-gray-500 w-[402.24px] h-[50.08px] bg-[#16171b] rounded-sm border-[0.40px] border-[#363636]"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
+                            placeholder="Password"
                         />
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-blue-600 text-white p-3 rounded-lg font-medium hover:bg-blue-700 transition duration-200"
+                        className="w-full bg-blue-600 text-white p-3 rounded-md font-medium hover:bg-blue-700 transition duration-200"
                     >
                         Login
                     </button>
